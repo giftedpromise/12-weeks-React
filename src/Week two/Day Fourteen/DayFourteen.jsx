16c2f897b7f996601af2870abba3a380300fe8e8
giftedpromise <giftedpromise1992@gmail.com>
2024-08-28 23:41:28 -1200

    day fourteen

---
+++ src/Week two/Day Fourteen/DayFourteen.jsx
@@ -0,0 +1,11 @@
+{
+  /*
+   Day 5: Context API
+Understanding the Context API
+The Context API is a React feature that allows you to share state or data across multiple components
+ without having to pass props down through each level of the component tree. 
+ This is particularly useful for managing global data that needs to be accessed by various 
+ parts of your application, such as user authentication status, themes, or language preferences. 
+    
+    */
+}
